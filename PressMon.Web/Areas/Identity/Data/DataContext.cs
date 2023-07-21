4327fa1357298d5655b374c5fd5d7739c3c14ed7
fadlillah <fadlillah@users.noreply.github.com>
2023-07-21 20:34:41 +0700

    Oke

--- PressMon.Web/Areas/Identity/Data/DataContext.cs
+++ PressMon.Web/Areas/Identity/Data/DataContext.cs
@@ -8,20 +8,17 @@ using Microsoft.EntityFrameworkCore;
 using PressMon.Web.Models;
 using PressMon.Web.Models;
 using TMS.Web.Apis;
-using PressMon.Web.Areas.Identity.Data;
 using TMS.Web.Models;
 
 namespace PressMon.Web.Data
 {
-    public class DataContext : IdentityDbContext<AppUser>
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options)
             : base(options)
         {        
         }
-        public DbSet<AppUser> AppUsers { get; set; }
-        public DbSet<AppRole> AppRoles { get; set; }
-        public DbSet<UserRole> userRoles { get; set; }
+
         public DbSet<LiveData> LiveDatas { get; set; }
         public DbSet<Alarm> Alarms { get; set; }
         public DbSet<Historical> Historicals { get; set; }
