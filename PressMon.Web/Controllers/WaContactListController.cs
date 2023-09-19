0862385717255ca1fb5d316261f1b7408325e93c
wahyurmd2100 <wahyurmd2100@gmail.com>
2023-09-19 16:35:22 +0700

    manual WA broadcast

--- PressMon.Web/Controllers/WaContactListController.cs
+++ PressMon.Web/Controllers/WaContactListController.cs
@@ -7,15 +7,27 @@ using System.Threading.Tasks;
 using TMS.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using DocumentFormat.OpenXml.InkML;
+using System.Net.Http;
+using DocumentFormat.OpenXml.Drawing;
+using TMS.Web.Models.sendWA;
+using System.Text.Json;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Path = System.IO.Path;
 
 namespace TMS.Web.Controllers
 {
     public class WaContactListController : Controller
     {
         private readonly DataContext _context;
-        public WaContactListController(DataContext context)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public WaContactListController(DataContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -35,12 +47,19 @@ namespace TMS.Web.Controllers
 
                 //get all data
                 var contact = (from p in _context.WaContactLists
-                              select new
-                              {
-                                  p.ContactID,
-                                  p.ContactName,
-                                  p.ContactNumber
-                              }).OrderBy(t => t.ContactID).Reverse();
+                               select new
+                               {
+                                   p.ContactID,
+                                   p.ContactName,
+                                   p.ContactNumber
+                               });
+
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    contact = contact.Where(d => d.ContactName.Contains(searchValue) || d.ContactNumber.Contains(searchValue));
+                }
+
+                contact = contact.OrderBy(t => t.ContactName);
 
                 //total number of rows counts   
                 recordsTotal = contact.Count();
@@ -127,20 +146,72 @@ namespace TMS.Web.Controllers
             return _context.WaContactLists.Any(e => e.ContactID == id);
         }
 
-        //[HttpPost]
-        //public IActionResult SendWA([FromBody] string numWA)
-        //{
-        //    try
-        //    {
-                
+        [HttpPost]
+        public async Task<IActionResult> SendWA(List<int> contactID)
+        {
+            var apiUrl = "https://multichannel.qiscus.com/whatsapp/v1/uwqks-1zpihp7ouot8pyv/4394/messages";
+            var httpClient = new HttpClient();
 
-        //        return Json(new { success = true, message = "WhatsApp message sent successfully." });
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Handle exceptions and errors here
-        //        return Json(new { success = false, message = ex.Message });
-        //    }
-        //}
+            httpClient.DefaultRequestHeaders.Add("Qiscus-App-Id", "uwqks-1zpihp7ouot8pyv");
+            httpClient.DefaultRequestHeaders.Add("Qiscus-Secret-Key", "407e74a091d93e91db35b5c8c1b0b708");
+            var responseMsg = "";
+
+            var pressPT01 = _context.LiveDatas.FirstOrDefault(n => n.LocationName == "M-01");
+            var pressPT02 = _context.LiveDatas.FirstOrDefault(n => n.LocationName == "M-02");
+            var timestamp = DateTime.Now.ToString("dd/MMM/yy HH:mm");
+
+            var numWAList = _context.WaContactLists
+                            .Where(c => contactID.Contains(c.ContactID))
+                            .Select(c => c.ContactNumber )
+                            .ToList();
+
+            try
+            {
+                foreach (var numWA in numWAList)
+                {
+                    // Prepare your data to be sent in the POST request
+                    var parameters = new List<Parameter>
+                    {
+                        new Parameter { type = "text", text = pressPT01.Pressure.ToString() }, // Replace "4.4" with your actual value
+                        new Parameter { type = "text", text = pressPT02.Pressure.ToString() }, // Replace "4.8" with your actual value
+                        new Parameter { type = "text", text = timestamp } // Replace with your actual date value
+                    };
+                    var components = new List<Component>
+                    {
+                        new Component { type = "body", parameters = parameters }
+                    };
+                    var language = new Language { policy = "deterministic", code = "id" };
+                    var template = new Template { @namespace = "6e85836a_6b63_48a8_9d2f_e8c8c365e157", name = "pressure_manual_send", language = language, components = components };
+                    var requestData = new RequestMessage() { to = numWA, type = "template", template = template };
+
+                    // Serialize the data to JSON
+                    var jsonPayload = JsonSerializer.Serialize(requestData);
+
+                    // Prepare the HTTP content with the JSON payload
+                    var httpContent = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+                    var response = await httpClient.PostAsync(apiUrl, httpContent);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        responseMsg = $"Response : {responseContent}";
+                    }
+                    else
+                    {
+                        responseMsg = $"Error : {response.StatusCode} - {response.ReasonPhrase}";
+                    }
+                }
+                
+                return Json(new { success = true, message = responseMsg });
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions and errors here
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+
     }
 }
---
+++ PressMon.Web/Models/sendWA/Response.cs
@@ -0,0 +1,22 @@
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using System.Collections.Generic;
+
+namespace TMS.Web.Models.sendWA
+{
+    public class Response
+    {
+        public List<contact> contacts { get; set; }
+        public List<message> messages { get; set; }
+        public string messaging_product { get; set; }
+    }
+    public class contact 
+    {
+        public string input { get; set; }
+        public string wa_id { get; set; }
+    }
+    public class message
+    {
+        public string id { get; set; }
+    }
+}
+
